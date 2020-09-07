7fc087957a27877dc750023abfc4578e1b29fd88
Marc Kohaupt <debuglevel@gmail.com>
2020-09-07 23:08:47 +0200

    Newlines between large logging blocks

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -8,11 +8,13 @@ PUPPET_REPO=$1
 HOSTNAME=$2
 BRANCH=$3
 
+echo
 echo "Setting hostname to ${HOSTNAME}..."
 hostname ${HOSTNAME}
 echo ${HOSTNAME} > /etc/hostname
 echo "Set hostname to ${HOSTNAME}"
 
+echo
 echo "Installing puppet..."
 source /etc/lsb-release
 apt-key adv --fetch-keys http://apt.puppetlabs.com/DEB-GPG-KEY-puppet
@@ -22,6 +24,7 @@ apt-get update
 apt-get -y install git puppet-agent
 echo "Installed puppet"
 
+echo
 echo "Cloning puppet repository '${PUPPET_REPO}'..."
 cd /etc/puppetlabs/code/environments
 mv production production.orig
@@ -30,14 +33,17 @@ cd production
 git checkout ${BRANCH}
 echo "Cloned puppet repository '${PUPPET_REPO}' and checked out branch ${BRANCH}"
 
+echo
 echo "Installing r10k..."
 /opt/puppetlabs/puppet/bin/gem install r10k --no-rdoc --no-ri
 echo "Installed r10k"
 
+echo
 echo "Installing Puppetfile dependencies..."
 /opt/puppetlabs/puppet/bin/r10k puppetfile install --verbose
 echo "Installed Puppetfile dependencies"
 
+echo
 echo "Applying puppet..."
 /opt/puppetlabs/bin/puppet apply --environment=production /etc/puppetlabs/code/environments/production/manifests/
 echo "Applied puppet"
