c77f226eae519b2eb62ac0ec343635d511baff3a
Marc Kohaupt <debuglevel@gmail.com>
2020-09-07 22:33:15 +0200

    Format puppify.sh

--- scripts/puppify.sh
+++ scripts/puppify.sh
@@ -1,22 +1,28 @@
 #!/bin/bash
-PUPPET_REPO=https://github.com/debuglevel/puppet-control.git
-IDENTITY="-i $HOME/.ssh/puppet.pem"
+
 if [ "$#" -lt 2 ]; then
   cat <<USAGE
-Usage: $0 TARGET HOSTNAME [BRANCH]
-Install Puppet on the node TARGET (IP address or DNS name) and run 
+Usage: $0 TARGETHOST HOSTNAME [BRANCH]
+Install Puppet on the node TARGETHOST (IP address or DNS name) and run 
 the bootstrap process. Set the hostname to HOSTNAME, and optionally use 
 the control repo branch BRANCH.
 USAGE
   exit 1
 fi
-TARGET=$1
+
+PUPPET_REPO=https://github.com/debuglevel/puppet-control.git
+IDENTITYFILE="-i $HOME/.ssh/puppet.pem"
+
+TARGETHOST=$1
 HOSTNAME=${2}
-BRANCH=${3:-production}
+BRANCH=${3:-production} # use "production" branch if none is provided
+
 OPTIONS="-oStrictHostKeyChecking=no"
+
 echo -n "Copying bootstrap script... "
-scp ${IDENTITY} ${OPTIONS} $(dirname $0)/bootstrap.sh ubuntu@${TARGET}:/tmp
+scp ${IDENTITYFILE} ${OPTIONS} $(dirname $0)/bootstrap.sh ubuntu@${TARGETHOST}:/tmp
 echo "done."
+
 echo -n "Bootstrapping... "
-ssh ${IDENTITY} ${OPTIONS} ubuntu@${TARGET} "sudo bash /tmp/bootstrap.sh ${PUPPET_REPO} ${HOSTNAME} ${BRANCH}"
+ssh ${IDENTITYFILE} ${OPTIONS} ubuntu@${TARGETHOST} "sudo bash /tmp/bootstrap.sh ${PUPPET_REPO} ${HOSTNAME} ${BRANCH}"
 echo "done."
