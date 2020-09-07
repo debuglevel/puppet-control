9b5464e0006982a13e34e2ca61d16060ffad436e
Marc Kohaupt <debuglevel@gmail.com>
2020-09-08 00:22:56 +0200

    TODO about UTC timezone

--- site-modules/profile/manifests/timezone.pp
+++ site-modules/profile/manifests/timezone.pp
@@ -1,4 +1,5 @@
 # Set the time zone for all nodes
+# TODO: Not sure about the UTC timezone; at least it should be configurable
 class profile::timezone {
   class { 'timezone':
     timezone => 'Etc/UTC',
