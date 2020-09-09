0c2d34df57b0204e1575f94f242c0602df57f788
Marc Kohaupt <marc.kohaupt@avm-d.de>
2020-09-09 13:12:14 +0200

    Refactoring

--- site-modules/profile/manifests/sudoers.pp
+++ site-modules/profile/manifests/sudoers.pp
@@ -5,7 +5,7 @@ class profile::sudoers {
     content  => 'Defaults      secure_path="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/opt/puppetlabs/puppet/bin:/opt/puppetlabs/bin"',
     priority => 0,
   }
-  $sudoers = lookup('sudoers', Array[String], 'unique', [])
+  $sudoers = lookup('sudo_users', Array[String], 'unique', [])
   $sudoers.each | String $user | {
     sudo::conf { $user:
       content  => "${user} ALL=(ALL) NOPASSWD: ALL",
