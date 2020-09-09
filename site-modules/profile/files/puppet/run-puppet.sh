f183280f93f81656df5d80878e33cbe74e7a284e
Marc Kohaupt <marc.kohaupt@avm-d.de>
2020-09-09 12:41:05 +0200

    Logging

--- site-modules/profile/files/puppet/run-puppet.sh
+++ site-modules/profile/files/puppet/run-puppet.sh
@@ -4,10 +4,13 @@
 cd /etc/puppetlabs/code/environments/production
 
 # update puppet control repo
+echo "git pull..."
 git pull
 
 # install dependencies
+echo "r10k puppetfile install..."
 /opt/puppetlabs/puppet/bin/r10k puppetfile install
 
 # apply puppet
+echo "puppet apply..."
 /opt/puppetlabs/bin/puppet apply --environment production manifests/
