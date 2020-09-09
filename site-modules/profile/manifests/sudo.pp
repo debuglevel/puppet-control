46e592af1095dda966fbe4445284b11578daebe0
Marc Kohaupt <marc.kohaupt@avm-d.de>
2020-09-09 13:13:45 +0200

    Refactoring

--- site-modules/profile/manifests/sudo.pp
+++ site-modules/profile/manifests/sudo.pp
@@ -1,12 +1,12 @@
 # Manage user privileges
 # TODO: I don't know what's the state of this config, but sudo should still ask for the user's password.
-class profile::sudoers {
+class profile::sudo {
   sudo::conf { 'secure_path':
     content  => 'Defaults      secure_path="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/opt/puppetlabs/puppet/bin:/opt/puppetlabs/bin"',
     priority => 0,
   }
-  $sudoers = lookup('sudo_users', Array[String], 'unique', [])
-  $sudoers.each | String $user | {
+  $users = lookup('sudo_users', Array[String], 'unique', [])
+  $users.each | String $user | {
     sudo::conf { $user:
       content  => "${user} ALL=(ALL) NOPASSWD: ALL",
       priority => 10,
